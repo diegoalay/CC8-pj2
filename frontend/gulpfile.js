73e0f60d74a8fcfb6582a9633c87044e456661b2
Json Chicas <rabelyj@gmail.com>
2019-10-28 11:48:57 -0600

    UPDATED: gulp file - gulp watch

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,11 +1,18 @@
 var fileinclude = require('gulp-file-include'),
     gulp = require('gulp');
 
-gulp.task('include', function() {
+gulp.task('fileinclude', function(done) {
     gulp.src(['src/templates/*.html'])
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
         }))
         .pipe(gulp.dest('./'));
+    done();
 });
+
+gulp.task('watch', function() {
+    gulp.watch(['src/**/*.html'], gulp.series('fileinclude'));
+});
+
+gulp.task('default', gulp.series(['watch', 'fileinclude']));
